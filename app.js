827e5454bc1da585ef56e2320178f033ccf110fb
IsraelImmanuel <100184074+IsraelImmanuel@users.noreply.github.com>
2022-06-11 17:07:13 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const btnReset = document.querySelector(".btn__reset");
+
+let missed = 0;
+
+btnReset.addEventListener('click', (e) => {
+  e.preventDefault();
+  overlay.style.display = "none";
+});
+
+//return a random phrase of an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+// adds the letters of the string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// check if a letter is in the phrase
+const checkLetter = button => {
+
+}
+
+// check if the game has been won or lost
+const checkWin = () => {
+
+}
+
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+
+});
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+});
